b109069dfc84c2b6f83875db5bd4c8cc2fc5c8f4
fatuk <fatukk@gmail.com>
2014-10-05 13:48:13 +0400

    Add home section

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,12 +1,14 @@
 module.exports = {
     dist: {
         options: {
-            sourceMap: true
+            sourceMap: false
         },
         files: {
             'vendors/plugins.min.js': [
                 'bower_components/jquery/dist/jquery.js',
-                'bower_components/fractionslider/jquery.fractionslider.min.js'
+                'bower_components/fractionslider/jquery.fractionslider.min.js',
+                'bower_components/underscore/underscore.js',
+                'bower_components/backbone/backbone.js'
             ]
         }
     }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,64 @@
 $(function() {
 
+    var App = {};
+    App.Views = {};
+    App.Collections = {};
+    App.Models = {};
+
+
+    /*****************
+     *
+     *  Views
+     *
+     ******************/
+
+    App.Views.App = Backbone.View.extend({
+        el: '.js-app',
+        initialize: function() {
+            var self = this;
+            this.getSizes();
+            $(window).on('resize', function(event) {
+                self.resize(event);
+            }).trigger('resize');
+        },
+        windowWidth: null,
+        windowHeight: null,
+        getSizes: function() {
+            this.$window = $(window);
+            this.windowWidth = this.$window.width();
+            this.windowHeight = this.$window.height();
+        },
+        setSizes: function() {
+            this.$('.js-home').css({
+                'width': this.windowWidth,
+                'height': this.windowHeight
+            });
+            this.$('#homeBgVideo').css({
+                // 'width': this.windowWidth,
+                // 'height': this.windowHeight
+            });
+        },
+        resize: function(e) {
+            var self = this;
+
+            $(window).off('resize');
+
+            setTimeout(function() {
+                self.getSizes();
+                self.setSizes();
+                $(window).on('resize', function(e) {
+                    self.resize(e);
+                });
+            }, 700);
+        }
+    });
+
+
+    /*****************
+     *
+     *  Initialize
+     *
+     ******************/
+
+    var appView = new App.Views.App();
 });
