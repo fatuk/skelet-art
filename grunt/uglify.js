bb84e72f6ec0060b2cfe7273f44b31c40d836207
fatuk <fatukk@gmail.com>
2014-10-08 23:18:55 +0400

    Add slider for projects

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -6,7 +6,7 @@ module.exports = {
         files: {
             'vendors/plugins.min.js': [
                 'bower_components/jquery/dist/jquery.js',
-                'bower_components/fractionslider/jquery.fractionslider.min.js',
+                'bower_components/jQuery-Open-Carousel/jquery.openCarousel.js',
                 'bower_components/underscore/underscore.js',
                 'bower_components/backbone/backbone.js'
             ]
